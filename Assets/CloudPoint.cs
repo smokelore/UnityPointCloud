24ac0e76c9ba75790a6f6ce34cd63eda0f9f797e
Sebastian Monroy <sebastian.monroy@gmail.com>
2013-04-10 13:03:10 -0400

    Chris's CloudPoint Script
    Chris created a CloudPoint class to contain the relevant information for
    each point in the point cloud.

---
+++ Assets/CloudPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using MathNet.Numerics.LinearAlgebra;
+
+public class CloudPoint
+{
+    public Color32 color { get; private set; }
+    public Vector location { get; private set; }
+    public Vector normal { get; private set; }
+
+    public CloudPoint(Vector location, Color32 color, Vector normal)
+    {
+        this.location = location;
+        this.color = color;
+        this.normal = normal;
+    }
+}
+
